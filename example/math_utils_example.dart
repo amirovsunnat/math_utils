3e7de6bef666bc4f804024cde868cf55fb3b2379
Ibrokhimovasss <sanobaribroximova@gmail.com>
2025-03-06 08:15:41 +0300

    Trigonometry has been added 2

--- example/math_utils_example.dart
+++ example/math_utils_example.dart
@@ -1,8 +1,1 @@
-import 'package:math_utils_n13/math_utils_n13.dart';
 
-void main() {
-  print(add(2, 3)); // 5
-  print(subtract(10, 4)); // 6
-  print(multiply(5, 2)); // 10
-  print(divide(8, 2)); // 4.0
-}
